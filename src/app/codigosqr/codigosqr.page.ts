74f1bacb1db4dd3eeae081f815c96a8f0ae11342
Danilo <da.bettancourt@duocuc.cl>
2024-10-12 09:53:14 -0300

    agregando cosas al qr

--- src/app/codigosqr/codigosqr.page.ts
+++ src/app/codigosqr/codigosqr.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { LoadingController, NavController, Platform } from '@ionic/angular';
+import html2canvas from 'html2canvas';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-codigosqr',
@@ -11,7 +14,11 @@ export class CodigosqrPage implements OnInit {
   segment = 'generate';
   qrText = '';
 
-  constructor(private navCtrl: NavController) {
+  constructor(
+    private navCtrl: NavController,
+    private loadingControler: LoadingController,
+    private platform: Platform
+   ) {
     
    }
    goBack() {
@@ -20,4 +27,48 @@ export class CodigosqrPage implements OnInit {
   ngOnInit() {
   }
 
+  // Captura la imagen para hacerla canva y luego obtener imagen
+  captureScreen(){
+    const element = document.getElementById('qrImage') as HTMLElement;
+
+    html2canvas(element).then((canvas: HTMLCanvasElement) => {
+      this.downloadImage(canvas);
+      if(this.platform.is('capacitor')) this.shareImage(canvas);
+      else this.downloadImage(canvas);
+    })
+  }
+
+  // Descargar imagen (web)
+  downloadImage(canvas: HTMLCanvasElement){
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL();
+    link.download = 'qr.png';
+    link.click();
+  }
+
+  // Compartir imagen (celular)
+  async shareImage(canvas:HTMLCanvasElement){
+    let base64 = canvas.toDataURL();
+    let path = 'qr.png';
+
+    const loading = await this.loadingControler.create({
+      spinner: 'crescent'
+    });
+    await loading.present();
+
+    await Filesystem.writeFile({
+      path,
+      data: base64,
+      directory: Directory.Cache
+    }).then(async (res) => {
+      let uri = res.uri;
+      await Share.share({url: uri});
+      await Filesystem.deleteFile({
+        path,
+        directory: Directory.Cache
+      })
+    }).finally(() => {
+      loading.dismiss();
+    })
+  }
 }
